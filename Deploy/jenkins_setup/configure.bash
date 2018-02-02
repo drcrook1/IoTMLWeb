7ca0d87a1e6debac61aa49ebf91de40a5f020bac
drcrook1 <drcrook@outlook.com>
2018-02-01 20:20:44 -0500

    trying again

--- Deploy/jenkins_setup/configure.bash
+++ Deploy/jenkins_setup/configure.bash
@@ -1,8 +1,14 @@
 sudo apt install docker.io
 sudo docker login iotml.azurecr.io -u iotml -p 3FjFUQK51wT/3TRXbHPjEmruCkGUUkFc
+sudo gpasswd -a jenkins docker
+sudo usermod -aG docker jenkins
+sudo service jenkins restart
 
 az aks install-cli
 az aks-getcredentials --resource-group=dacrook-iotmlweb --name=dacrookiotmlcluster
+
+sudo mkdir /var/lib/jenkins/.kube
+sudo cp ~/.kube/config /var/lib/jenkins/.kube/config
 
 git clone https://github.com/drcrook1/IoTMLWeb.git
 
